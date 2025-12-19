94a3d560ca8e0799dbf9ce4a02ed6c99ad370257
Zack Clobes <zack@custom-ds.com>
2025-12-18 20:51:33 -0600

    Firmware approaching release candidate.

--- source/Vehicle-Auto-On/poc/proofofconcept.cpp
+++ source/Vehicle-Auto-On/poc/proofofconcept.cpp
@@ -9,6 +9,18 @@
 #define F_CPU 3333333U
 
 
+// Function prototypes
+bool initLIS2HH12();
+bool checkForMotion();
+void writeLIS2HH12Register(uint8_t reg, uint8_t value);
+uint8_t readLIS2HH12Register(uint8_t reg);
+
+void customSerialInit();
+void customSerialWrite(uint8_t data);
+void customSerialPrint(const char* str);
+void customSerialPrintln(const char* str);
+void customSerialPrint(uint16_t num);
+
 
 //Outputs
 #define LED_STATUS PIN_PA6
@@ -25,19 +37,12 @@
 #define ANALOG_REFERENCE PIN_PA2
 
 //Software Serial pins
-#define DEBUG_SERIAL
+//define DEBUG_SERIAL
 #define SOFT_SERIAL_TX PIN_PB3
 
 //Constants
 #define MAX_COUNDOWN 2400   //Half-second intervals to keep the relay on after losing battery voltage sense. Set to 20 minutes (2400 * 0.5s)
 //define MAX_COUNDOWN 10   //Half-second intervals to keep the relay on after losing battery voltage sense --- IGNORE ---
-#define SWITCH_POSITION_DELAY 3
-
-// How many cycles to wait between events. Each cycle is about 300mS
-
-#define CHECK_AUTO_INTERVAL 9   //Number of loop cycles between auto-checks for charging/motion. 9 = 3.0 seconds
-#define AUTO_SHUTDOWN_DELAY 30 //1800 //Number of cycles to wait before auto-shutdown when not charging or in motion. 1800 = about 10 minutes
-
 
 // LIS2HH12 accelerometer constants
 #define LIS2HH12_I2C_ADDRESS 0x1D  // Default I2C address (can also be 0x1D depending on SA0 pin) (0x1E is default, 0x1D if SA0 is grounded)
@@ -75,22 +80,10 @@ enum SwitchState {
   STATE_AUTO
 };
 
-enum LightState {
-  LIGHT_OFF,
-  LIGHT_ON,
-  BLINK_SLOW,
-  BLINK_FAST
-};
-
 
 // Function prototypes
-bool isCharging();
-bool isInMotion();
-
-void setupRTC();
-void goToSleep(bool lowPowerMode);
-
 bool initLIS2HH12();
+bool checkForMotion();
 void writeLIS2HH12Register(uint8_t reg, uint8_t value);
 uint8_t readLIS2HH12Register(uint8_t reg);
 int16_t readLIS2HH12Axis(uint8_t regL, uint8_t regH);
@@ -133,6 +126,8 @@ void setup()
   // Initialize custom bit-banged serial on PB2 (TX) and PB3 (RX)
   // Use a lower baud rate for better reliability with ATtiny404 at 3.33MHz
   customSerialInit();  // Initialize custom serial at 1200 baud
+
+
   #ifdef DEBUG_SERIAL
   customSerialPrintln("Start..");
   #endif
@@ -149,24 +144,24 @@ void setup()
   customSerialPrintln("done");
   #endif
 
-  setupRTC();
+  //Set up WDT for ATtiny404
+  // cli(); // Disable interrupts during WDT setup
+  // Configure WDT in interrupt mode (not reset mode)
+  // CCP = CCP_IOREG_gc; // Enable protected register write
+  // WDT.CTRLA = WDT_PERIOD_512CLK_gc; // ~512ms timeout, interrupt mode
+  //sei(); // Re-enable interrupts
+
+  set_sleep_mode(SLEEP_MODE_STANDBY); // Use STANDBY for WDT wake-up
 }
 
-
-
-
-void loop() {
-  SwitchState nextPosition;
-
-  //static vars to survive the sleep cycles
-  static LightState ledMode;
-  static uint16_t nextPositionDelay = 0;
-  static uint16_t autoReaffirm = 0;
-  static uint16_t autoKeepAliveCount = 0;
-  static uint16_t ledCount = 0;
+void loop()
+{
+  SwitchState switchPosition;
+  uint16_t battLevel, refLevel;
 
   static int autoCountdown;
-  static bool lowPower = false;
+  static bool ledState = false;
+  static bool relayState = false;
 
 
   //Check the switch state
@@ -174,227 +169,170 @@ void loop() {
   pinMode(SWITCH_ON, INPUT_PULLUP);  
 
 
-  //Assume it's safe to go into low-power mode
-  lowPower = true;
-
-
   //Determine the switch position
   if (digitalRead(SWITCH_AUTO) == HIGH && digitalRead(SWITCH_ON) == HIGH) {
-    //both high, so it's in the off position
-    if (nextPosition != STATE_OFF) {
-      nextPosition = STATE_OFF;
-      nextPositionDelay = SWITCH_POSITION_DELAY; //delay 5 cycles before changing state
-    }
+    switchPosition = STATE_OFF;
   } else if (digitalRead(SWITCH_ON) == LOW) {
     //Switch is in the manual on position
-    if (nextPosition != STATE_ON) {
-      nextPosition = STATE_ON;
-      nextPositionDelay = SWITCH_POSITION_DELAY; //delay 5 cycles before changing state
-    }
+    switchPosition = STATE_ON;
   } else if (digitalRead(SWITCH_AUTO) == LOW) {
     //Switch is in auto position
-    if (nextPosition != STATE_AUTO) {
-      nextPosition = STATE_AUTO;
-      nextPositionDelay = SWITCH_POSITION_DELAY; //delay 5 cycles before changing state
-    }
+    switchPosition = STATE_AUTO;
   }
+  
+  
   
 
 
-  if (nextPositionDelay > 0) {
-    nextPositionDelay--;
-  } else {
-    //it's time to make a move
 
-    switch (nextPosition) {
+  
+  switch (switchPosition) {
     case STATE_OFF:
       // Turn off the relay and LED
       #ifdef DEBUG_SERIAL
-      customSerialPrintln("0");
+      customSerialPrintln("OF");
       #endif
 
       digitalWrite(RELAY, LOW);
-      ledMode = LIGHT_OFF;
-      ledCount = 0;
-      lowPower = true;    // allow it to go into low power mode
+      relayState = false;
+      digitalWrite(LED_STATUS, LOW);
+      ledState = false;
 
-      autoKeepAliveCount = 0;  //reset the auto keep-alive counter
+      checkForMotion();  //just check for motion to keep the accelerometer active
 
       break;
+
     case STATE_ON:
       // Turn on the relay and LED
       #ifdef DEBUG_SERIAL
-      customSerialPrintln("1");
+      customSerialPrintln("ON");
       #endif
+
       digitalWrite(RELAY, HIGH);
-      ledMode = LIGHT_ON;
-      ledCount = 0;
-      lowPower = false;   //stay awake when manually on
-
-      autoKeepAliveCount = 0;  //reset the auto keep-alive counter
-
+      relayState = true;
+      digitalWrite(LED_STATUS, HIGH);
+      ledState = true;
       break;
+
     case STATE_AUTO:
       // In auto mode, check for battery voltage, and motion if we're already switched on
       #ifdef DEBUG_SERIAL
-      customSerialPrintln("A");
+      customSerialPrintln("AU");
+      //customSerialPrintln("Read Analog");
       #endif
 
-      if (autoReaffirm > 0) {
-        autoReaffirm--;
+
+      digitalWrite(ANALOG_ENABLE, HIGH);    //enable the analog circuitry
+      delay(25);    //wait for it to stabilize
+      battLevel = analogRead(ANALOG_BATTERY_LEVEL);
+      refLevel = analogRead(ANALOG_REFERENCE);
+      //turn off the analog enable pin
+      digitalWrite(ANALOG_ENABLE, LOW);     
+
+
+      #ifdef DEBUG_SERIAL
+      customSerialPrint("Bat ");
+      customSerialPrint(battLevel);
+      customSerialPrintln("");
+      customSerialPrint("Ref  ");
+      customSerialPrint(refLevel);
+      customSerialPrintln("");
+      #endif
+
+      //Desensitize the reference level a bit to avoid noise
+      refLevel = refLevel / 4;  //divide by 4 to reduce noise sensitivity
+      refLevel = refLevel + 526; //311 is about 11.5V on the battery sense line
+
+      #ifdef DEBUG_SERIAL
+      customSerialPrint("CRef ");
+      customSerialPrint(refLevel);
+      customSerialPrintln("");
+      #endif
+
+
+      bool motion = checkForMotion();
+      if (motion) {
+        //Motion detected - keep the relay on
+        #ifdef DEBUG_SERIAL
+        customSerialPrintln("*");
+        #endif
+
       } else {
-        //it's time to reaffirm if the car is charging/moving
-        if (isCharging()) {
-          //the car battery is charging - stay awake
-          autoReaffirm = CHECK_AUTO_INTERVAL;
-          if (ledMode != BLINK_SLOW) {
-            ledMode = BLINK_SLOW;
-            ledCount = 0;
-          }
-          //reset the autoKeepAliveCount
-          autoKeepAliveCount = AUTO_SHUTDOWN_DELAY;
-
-        } else {
-          //maybe it's moving but the battery voltage is dropping - this happens on modern cars with smart alternators
-
-          //but make sure that the Auto had previously been woken due to charging, before failing back to motion detection for keep-alive
-          if (autoKeepAliveCount > 0) {
-            if (isInMotion()) {
-              //the car is moving - stay awake
-              autoReaffirm = CHECK_AUTO_INTERVAL;
-              if (ledMode != BLINK_FAST) {
-                ledMode = BLINK_FAST;
-                ledCount = 0;
-              }
-
-              //reset the autoKeepAliveCount
-              autoKeepAliveCount = AUTO_SHUTDOWN_DELAY;
-            } else {
-              // Not charging and not in motion - start the shutdown process
-              ledMode = LIGHT_OFF;
-              ledCount = 0;
-              // autoShutdown
-            }
-          }
-        }
+        #ifdef DEBUG_SERIAL
+        customSerialPrintln("-");
+        #endif
       }
 
-      //Check the state of the relay based on the autoKeepAliveCount
-      if (autoKeepAliveCount > 0) {
-        autoKeepAliveCount--;   //decrement the keep-alive counter while it's positive
-        digitalWrite(RELAY, HIGH);
-        lowPower = false;   //stay awake while the relay is on
-      } else {
-        digitalWrite(RELAY, LOW);
-        lowPower = true;    //allow low power mode when the relay is off
-      }
 
-      break;
-    }
+      //check the input voltage to see if the alternator is charging the battery
+      if (battLevel > refLevel)
+      {
+        //We have positive voltage on the input sense - reset the autoCountdown
+        #ifdef DEBUG_SERIAL
+        customSerialPrintln("Chrg");
+        #endif
 
-
-    int8_t currentLEDState = digitalRead(LED_STATUS);
-    //Check the LED Blink Status
-    if (ledCount == 0) {
-      //it's time to cycle the LED
-      switch (ledMode) {
-      case LIGHT_OFF:
-        digitalWrite(LED_STATUS, LOW);
-        ledCount = 0xFFFF; //stay off for quite a while
-        break;
-      case LIGHT_ON:
+        digitalWrite(RELAY, HIGH);    //turn on the relay
+        relayState = true;
         digitalWrite(LED_STATUS, HIGH);
-        ledCount = 0xFFFF; //stay on for quite a while
-        break;
-      case BLINK_SLOW:
-        digitalWrite(LED_STATUS, !currentLEDState);
-        ledCount = 6; //slow blink
-        break;
-      case BLINK_FAST:
-        digitalWrite(LED_STATUS, !currentLEDState);
-        ledCount = 2; //fast blink
-        break;
+        ledState = true;
+        autoCountdown = MAX_COUNDOWN;   //reset the timer to keep the relay on even after the battery voltage drops
       }
+      else
+      {
+        //We have negative voltage on the input sense - decrement the autoCountdown
+        if (autoCountdown > 0) 
+        {
+          #ifdef DEBUG_SERIAL
+          customSerialPrintln("Coast");
+          #endif
+          
+          autoCountdown--;
+          //Blink the LED if we were previously on, but now in coast because the voltage has dropped
+          ledState = !ledState;
+          digitalWrite(LED_STATUS, ledState);
+        }
+        else
+        {
+          #ifdef DEBUG_SERIAL
+          customSerialPrintln(">0");
+          #endif
 
-    } else {
-      ledCount--;
-    }
-
+          //Turn off the relay
+          digitalWrite(RELAY, LOW);
+          relayState = false;
+          digitalWrite(LED_STATUS, LOW);
+          ledState = false;
+        }
+      }   
+      break;
   }
-
-
-
-
-  goToSleep(lowPower);
-}
-
-ISR(RTC_CNT_vect) {
-  RTC.INTFLAGS = RTC_OVF_bm; // clear flag
-}
-
-ISR(RTC_PIT_vect) {
-  RTC.PITINTFLAGS = RTC_PI_bm; // clear PIT interrupt flag
-}
-
-
-
-void setupRTC() {
-  while (RTC.STATUS > 0) {}               // wait for sync
-  RTC.CLKSEL = RTC_CLKSEL_INT32K_gc;      // 32.768 kHz internal ULP oscillator
-  RTC.PITINTCTRL = RTC_PI_bm;             // enable PIT interrupt
-  //RTC.PITCTRLA   = RTC_PERIOD_CYC32768_gc | RTC_PITEN_bm;        // start PIT   // About 1 second
-  //RTC.PITCTRLA   = RTC_PERIOD_CYC16384_gc | RTC_PITEN_bm;        // start PIT   // About 500mS
-  RTC.PITCTRLA   = RTC_PERIOD_CYC8192_gc | RTC_PITEN_bm;        // start PIT    // About 250mS
-}
-
-
-void goToSleep(bool lowPowerMode) {
-
-  if (lowPowerMode) {
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  sleep_enable();
-  sei();
-  sleep_cpu();
-  sleep_disable();
-  } else {
-    delay(250);
-  }
-
-}
-
-
-bool isCharging() {
-  uint16_t battLevel, refLevel;
-
-
-  //read the analog inputs
-  digitalWrite(ANALOG_ENABLE, HIGH);    //enable the analog circuitry
-  delay(25);    //wait for it to stabilize
-  battLevel = analogRead(ANALOG_BATTERY_LEVEL);
-  refLevel = analogRead(ANALOG_REFERENCE);
-  //turn off the analog enable pin
-  digitalWrite(ANALOG_ENABLE, LOW);     
-
-
-
-  //Desensitize the reference level a bit to avoid noise
-  refLevel = refLevel / 4;  //divide by 4 to reduce noise sensitivity
-  refLevel = refLevel + 526; //311 is about 11.5V on the battery sense line
 
   #ifdef DEBUG_SERIAL
-  customSerialPrint("Bat ");
-  customSerialPrint(battLevel);
-  // customSerialPrintln("");
-  // customSerialPrint("Ref  ");
-  // customSerialPrint(refLevel);
-  // customSerialPrintln("");      
-  customSerialPrint("CRef ");
-  customSerialPrint(refLevel);
+  customSerialPrintln("");
   customSerialPrintln("");
   #endif
 
-  return (battLevel > refLevel);
+
+
+
+  delay(500);
+
+  // __asm__ __volatile__ ("wdr");   //similar to wdt_reset() but doesn't mess with the WDT config causing a full reset at overflow
+  // if (ledState || relayState) {
+  //   //we have something turned on. just delay for 500ms
+  //   delay(500);
+  // } else {
+  //   //Sleep for about 500ms in low power mode
+  //   // Prepare for WDT wake-up sleep
+  //   sleep_enable();
+  //   sei(); // Ensure interrupts are enabled for WDT wake-up
+  //   sleep_cpu(); // Sleep until WDT interrupt
+  //   sleep_disable();
+  //   cli(); // Disable interrupts after waking up - no need for these until we go back to sleep again
+  // }
 }
+
 
 
 
@@ -433,6 +371,9 @@ bool initLIS2HH12() {
   customSerialPrint(whoAmI);
 
   if (whoAmI != 0x41) {  // Expected WHO_AM_I value for LIS2HH12
+    #ifndef DEBUG_SERIAL
+    customSerialPrintln("miss");
+    #endif
 
     digitalWrite(RELAY, HIGH); //debug
     delay(250);
@@ -457,12 +398,31 @@ bool initLIS2HH12() {
   prevAccelZ = readLIS2HH12Axis(LIS2HH12_OUT_Z_L, LIS2HH12_OUT_Z_H);
   
   accelInitialized = true;
+  digitalWrite(RELAY, HIGH); //debug
+  delay(250);
+  digitalWrite(RELAY, LOW);  //debug
+  delay(250);
+  digitalWrite(RELAY, HIGH); //debug
+  delay(250);
+  digitalWrite(RELAY, LOW);  //debug
+  delay(250);  
+  digitalWrite(RELAY, HIGH); //debug
+  delay(250);
+  digitalWrite(RELAY, LOW);  //debug
+  delay(250);
 
+  #ifdef DEBUG_SERIAL
+  customSerialPrintln("...");
+  #endif
   return true;
 }
 
-bool isInMotion() {
+bool checkForMotion() {
   //Check the LIS2HH12 accelerometer for motion
+  digitalWrite(LED_STATUS, HIGH); // Indicate we're checking
+  delay(500);
+  digitalWrite(LED_STATUS, LOW);
+  delay(500);
 
   if (!accelInitialized) {
     return false; // Accelerometer not initialized
@@ -486,6 +446,27 @@ bool isInMotion() {
   prevAccelY = currentAccelY;
   prevAccelZ = currentAccelZ;
   
+  // Optional: Print debug information
+  if (motionDetected) {
+    digitalWrite(LED_STATUS, HIGH); // Indicate motion detected
+    delay(4000);
+    digitalWrite(LED_STATUS, LOW);
+    delay(500);
+    #ifdef DEBUG_SERIAL
+    customSerialPrint("X: ");
+    customSerialPrint(String(deltaX).c_str());
+    customSerialPrint(", Y: ");
+    customSerialPrint(String(deltaY).c_str());
+    customSerialPrint(", Z: ");
+    customSerialPrintln(String(deltaZ).c_str());
+    #endif
+  }
+  
+  digitalWrite(LED_STATUS, HIGH); // Indicate we're checking
+  delay(150);
+  digitalWrite(LED_STATUS, LOW);
+  delay(2000);
+
   return motionDetected;
 }
 
